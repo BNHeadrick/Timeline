620f673a680ae3e0a964623797b77c5c7f2934f5
Brandon <Hozuko@gmail.com>
2012-11-12 12:18:04 -0500

    added addPage functionality to the addPage image

--- TimeLineViewController.h
+++ TimeLineViewController.h
@@ -11,14 +11,24 @@
 #import "TimeLineView.h"
 
 @interface TimeLineViewController : UIViewController
+{
+  UILabel *statusLabel;
+  //UIImageView* addPageImage;
+}
+@property (strong, nonatomic) IBOutlet UILabel *statusLabel;
 
 @property (strong, nonatomic) Timeline* timeline;
 @property (strong, nonatomic) TimeLineView* timeView;
 @property (strong, nonatomic) UIView *tlView;
 @property (strong, nonatomic) UIView *apView;
+//@property (strong, nonatomic) UIImageView* addPageImage;
 
 -(id)init;
 
 -(IBAction)click:(UIButton *)sender;
+//-(IBAction)tap:(UIView *)sender;
+
+-(IBAction)controlPan:(UIPanGestureRecognizer *)recognizer;
+-(IBAction)addPageImage:(UIPanGestureRecognizer *)recognizer;
 
 @end
--- TimeLineViewController.m
+++ TimeLineViewController.m
@@ -18,7 +18,41 @@
 @implementation TimeLineViewController
 @synthesize timeline;
 @synthesize timeView;
+@synthesize statusLabel;
 
+
+-(IBAction)controlPan:(UIPanGestureRecognizer *)recognizer{
+  CGPoint translation = [recognizer translationInView:self.view];
+  recognizer.view.center = CGPointMake(recognizer.view.center.x + translation.x, recognizer.view.center.y + translation.y);
+  [recognizer setTranslation:CGPointMake(0, 0) inView:self.view];
+}
+
+-(IBAction)addPageImage:(UITapGestureRecognizer *)recognizer:(UITapGestureRecognizer *)recognizer{
+
+  
+  [timeline addPage];
+  
+  NSLog(@"num of pages is %d", [[timeline pages] count]);
+  
+  [(TimeLineView *)_tlView setNumLines:[[timeline pages] count]];
+  
+  [_tlView setNeedsDisplay];
+}
+
+/*
+-(IBAction)tap:(UIPanGestureRecognizer *)recognizer{
+  CGPoint translation = [recognizer translationInView:self.view];
+  recognizer.view.center = CGPointMake(recognizer.view.center.x + translation.x, recognizer.view.center.y + translation.y);
+  [recognizer setTranslation:CGPointMake(0, 0) inView:self.view];
+  
+  UITapGestureRecognizer *doubleTap =
+  [[UITapGestureRecognizer alloc]
+   initWithTarget:self
+   action:@selector(tapDetected:)];
+  doubleTap.numberOfTapsRequired = 2;
+  [self.view addGestureRecognizer:doubleTap];
+}
+ */
 
 - (void)viewDidLoad
 {
@@ -28,7 +62,59 @@
   
   _tlView = (UIView *)[self.view viewWithTag:1];
   _apView = (UIView *)[self.view viewWithTag:2];
+  //_addPageImage = (UIImage *)[self.view viewWithTag:3];
   
+  /***
+  //diff gesture stuff
+  addPageImage.userInteractionEnabled = YES;
+  
+  
+//  UIPanGestureRecognizer *panRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(panDetected:)];
+//  [self.imageView addGestureRecognizer:panRecognizer];
+  
+
+  
+  UITapGestureRecognizer *tapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleSingleTap:)];
+  tapRecognizer.numberOfTapsRequired = 1;
+  [addPageImage addGestureRecognizer:tapRecognizer];
+  ***/
+  
+  //gesture code stuffs below
+  /*
+  UITapGestureRecognizer *doubleTap =
+  [[UITapGestureRecognizer alloc]
+   initWithTarget:self
+   action:@selector(tapDetected:)];
+  doubleTap.numberOfTapsRequired = 2;
+  [self.view addGestureRecognizer:doubleTap];
+  
+  UIPinchGestureRecognizer *pinchRecognizer =
+  [[UIPinchGestureRecognizer alloc]
+   initWithTarget:self
+   action:@selector(pinchDetected:)];
+  [self.view addGestureRecognizer:pinchRecognizer];
+  
+  UIRotationGestureRecognizer *rotationRecognizer =
+  [[UIRotationGestureRecognizer alloc]
+   initWithTarget:self
+   action:@selector(rotationDetected:)];
+  [self.view addGestureRecognizer:rotationRecognizer];
+  
+  UISwipeGestureRecognizer *swipeRecognizer =
+  [[UISwipeGestureRecognizer alloc]
+   initWithTarget:self
+   action:@selector(swipeDetected:)];
+  swipeRecognizer.direction = UISwipeGestureRecognizerDirectionRight;
+  [self.view addGestureRecognizer:swipeRecognizer];
+  
+  UILongPressGestureRecognizer *longPressRecognizer =
+  [[UILongPressGestureRecognizer alloc]
+   initWithTarget:self
+   action:@selector(longPressDetected:)];
+  longPressRecognizer.minimumPressDuration = 3;
+  longPressRecognizer.numberOfTouchesRequired = 1;
+  [self.view addGestureRecognizer:longPressRecognizer];
+  */
 }
 
 - (void)didReceiveMemoryWarning
@@ -77,5 +163,46 @@
   //[[[timeline pages] objectAtIndex:0] setNeedsDisplay];
   
 }
+/*
+- (IBAction)longPressDetected:(UIGestureRecognizer *)sender {
+  statusLabel.text = @"Long Press";
+}
+
+- (IBAction)swipeDetected:(UIGestureRecognizer *)sender {
+  statusLabel.text = @"Right Swipe";
+}
+
+- (IBAction)tapDetected:(UIGestureRecognizer *)sender {
+  statusLabel.text = @"Double Tap";
+}
+
+- (IBAction)pinchDetected:(UIGestureRecognizer *)sender {
+  
+  CGFloat scale =
+  [(UIPinchGestureRecognizer *)sender scale];
+  CGFloat velocity =
+  [(UIPinchGestureRecognizer *)sender velocity];
+  
+  NSString *resultString = [[NSString alloc] initWithFormat:
+                            @"Pinch - scale = %f, velocity = %f",
+                            scale, velocity];
+  statusLabel.text = resultString;
+}
+
+
+- (IBAction)rotationDetected:(UIGestureRecognizer *)sender {
+  CGFloat radians =
+  [(UIRotationGestureRecognizer *)sender rotation];
+  CGFloat velocity =
+  [(UIRotationGestureRecognizer *)sender velocity];
+  
+  NSString *resultString = [[NSString alloc] initWithFormat:
+                            @"Rotation - Radians = %f, velocity = %f",
+                            radians, velocity];
+  statusLabel.text = resultString;
+}
+*/
+
+
 
 @end
