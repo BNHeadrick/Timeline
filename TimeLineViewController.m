f06e129b755877b170d97f1a762d70a783c78e1b
Brandon <Hozuko@gmail.com>
2012-11-13 23:59:54 -0500

    Added panning functionality to setting active pages

--- TimeLineViewController.m
+++ TimeLineViewController.m
@@ -40,6 +40,41 @@
   
   NSLog(@"PannedTime");
   
+  //NSLog(@"TouchedTimeline");
+  
+  //TODO; make this get the size of the view object, not hardcoded width based on the storyboard attribute.
+  CGRect screenRect = [[UIScreen mainScreen] bounds];
+  //  CGFloat screenWidth = screenRect.size.width-108;
+  CGFloat screenWidth = screenRect.size.height-108;
+  
+  //the below code is gross...I will get to making it cleaner later.  For now, I'm moving on.
+  //nieve way to get the subdivision of the UIView area; draws right to left
+  NSLog(@"numlines is %d",[(TimeLineView *)_tlView numLines]);
+  int numLines = [(TimeLineView *)_tlView numLines];
+  int displacement = screenWidth/numLines;
+  int absLoc = screenWidth;
+  int activeIndex = [(TimeLineView *)_tlView numLines]-1;
+  int tempActive = -1;
+  if(numLines>0){
+    for(int i = 0; i<numLines-1; i++){
+      tempActive = [(TimeLineView *)_tlView numLines]-2;
+      absLoc = absLoc - displacement;
+      
+      CGPoint location = [recognizer locationInView:self.view];
+      
+      if(location.x < absLoc){
+        //NSLog(@"trans %f%f",location.x,location.y);
+        tempActive = tempActive - i;
+        activeIndex = tempActive;
+        //NSLog(@"trans %f  %f  %d",location.x,location.y,tempActive);
+      }
+      
+    }
+    //activeIndex = tempActive;
+    [timeline setActivePageWithIndex:activeIndex];
+    NSLog(@"activeIs %d",activeIndex);
+  }
+  
   
 }
 
